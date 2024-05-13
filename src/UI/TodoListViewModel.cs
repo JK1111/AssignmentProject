7d56bd5f5d1b456cb76652cc44fa1d6d3ecbbc4c
Jelena Kovacevic <jelena.kovacevic@enlightit.rs>
2024-05-13 07:48:10 +0200

    Show error message when user attempt to enter Todo List with Title that already exists or use Title with more than 200 characters

--- src/UI/TodoListViewModel.cs
+++ src/UI/TodoListViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Input;
+﻿using System.Windows;
+using System.Windows.Input;
+using Assignment.Application.Common.Exceptions;
 using Assignment.Application.TodoLists.Commands.CreateTodoList;
 using Caliburn.Micro;
 using MediatR;
@@ -32,7 +34,27 @@ public class TodoListViewModel : Screen
 
     private async void SaveExecute(object parameter)
     {
-        await _sender.Send(new CreateTodoListCommand(Title));
+        try
+        {
+            await _sender.Send(new CreateTodoListCommand(Title));
+        }
+        catch (ValidationException ex)
+        {
+            if (ex.Errors.Any())
+            {
+                string message = null;
+
+                if (ex.Errors.ContainsKey("Title"))
+                {
+                    message = ex.Errors["Title"].First();
+                }
+                MessageBox.Show(message, "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
         await TryCloseAsync(true);
     }
 
